21d81edb696187e3e87cbeea8f5937bde849068f
phcc321018 <phcc32@student.cis.edu.hk>
2023-10-04 08:57:10 +0800

    lab 1.1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        // Dating Age Rule
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        int datingAge = 7 + (age / 2);
+        System.out.println(age + "-year olds should date somebody who is at least " + datingAge + " years old.");
+
+        scanner.close();
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Dating Age Rule
+        System.out.print("Enter your age: ");
+
+        int age = scanner.nextInt();
+        // Book Pages Rule
+        int pagesToRead = 100 - age;
+        System.out.println(age + "-year olds should read at least " + pagesToRead + " pages before giving up on a book.");
+
 
     }
 }
